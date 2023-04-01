2dd6f18a1b74f6ad2d1dbe2f6afa6006eff7024b
Phanrazak <Phanrazak@yahoo.com>
2023-04-02 00:59:13 +0800

    Removed some unneeded stuff

--- Commands/DiscordSelectComponentInteraction.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Suzuna_Chan_2.commands
-{
-    internal class DiscordSelectComponentInteraction
-    {
-    }
-}
--- Commands/discord.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Suzuna_Chan_2.commands
-{
-    internal class discord
-    {
-        public static System.Func<object, Task> ComponentInteractionCreated { get; internal set; }
-    }
-}
